1df8e7cc5a23d2ac8a5e44a069e5cf4ca20e64f9
YamuLema <flyleaf_yecc_krb@hotmail.com>
2014-06-24 19:41:12 -0500

    Programen
    Los quiero bitches

---
+++ metadatos.php
@@ -0,0 +1,11 @@
+<head>
+    <title>Check ESPOL</title>
+    <link href="Bootstrap/bootstrap.css" rel="stylesheet" />
+    <link href="Bootstrap/estilo.css" rel="stylesheet" />
+    <script src="js/jquery.min.js"></script>  
+    <script src="js/bootstrap.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Frijole' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Bitter:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+</head>
