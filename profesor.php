131f8ac4c85d26f67b1511e9c22fedb7d6b9bd66
Josty <josty-12@hotmail.com>
2014-07-29 10:16:01 -0500

    Update profesor.php

--- profesor.php
+++ profesor.php
@@ -1,47 +1,4 @@
-<?php
-
-class Profesor{
-  private $localhost = "localhost";    
-    private $usuario = "root";
-    private $password = "root";
-    private $database = "Profesor"; 
-
-public function conectar()
- {
-  if(!isset($this->conexion)){
-    $this->conexion = (mysql_connect($this->localhost, $this->usuario,$this->password)) or die(mysql_error() );
-    mysql_select_db($this->database , $this->conexion) or die(mysql_error());      
-
-  }
- }     
-
-public function consulta($sql)
- {
-    $muestra = mysql_query($sql,$this->conexion);
-    if(!$resultado){
-     echo 'MySQL Error: ' . mysql_error();
-     exit;
-    }
-    return $muestra; 
- }
- }
-
-
-$conn= mysql_connect("localhost", "root", "") or die (mysql_error());
-mysql_select_db("prueba", $conn);
-$sql = "select * from Profesor";
-$result = mysql_query($sql, $conn) or die (mysql_error());
-
-while($row = mysql_fetch_assoc($result)) {
-  foreach ($row as $name => $value) {
-    print "$name: $value <br /> \n";
-    }
-
-}
-
-
-
-
+<?
 class Profesor { 
 
 	private $nombre;
