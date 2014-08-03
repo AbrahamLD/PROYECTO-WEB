f75e1e638180811d1f37dd7dbfad33a1b75beb0e
Hernan Quinde <andres.ra22@gmail.com>
2014-08-03 18:37:47 -0500

    Registro.php
    Guarda con la validaciones de un correo existente

---
+++ registro.php
@@ -0,0 +1,30 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$apodo = $_POST['apodo'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+{
+	$conn= mysql_connect("localhost", "root", "") or die (mysql_error());
+	mysql_select_db("clasedemo", $conn);
+	$verificaEmail = mysql_query("SELECT * FROM registro WHERE email = '$email'"); 
+		
+      if (mysql_num_rows ($verificaEmail) > 0) { 
+           echo'<script type="text/javascript">
+                alert("Error al registrar! - Correo Duplicado - Ingresa otro");
+                window.location="http://localhost/registrarse/creaciondeusuarioalterna.html"
+                </script>';
+      }
+      else
+      { 
+		mysql_query("INSERT INTO registro (nombre,apellido,apodo,password,email) VALUES ('$nombre','$apellido','$apodo',sha1(md5('$password')),'$email')",$conn ); 
+		echo echo'<script type="text/javascript">
+                alert("El Registro se ha completado con exito");
+                window.location="http://localhost/registrarse/creaciondeusuarioalterna.html"
+                </script>';
+	  }
+}
+
+
+?>
