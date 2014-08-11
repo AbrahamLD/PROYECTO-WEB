46a68a9a038e12aee7e00e9dfff59a73fd786048
Hernan Quinde <andres.ra22@gmail.com>
2014-08-11 00:21:33 -0500

    login

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <title>Check Login</title>
+ <meta charset = "utf8" />
+</head>
+
+<body>
+
+<?php
+
+	$conn= mysql_connect("localhost", "root", "") or die (mysql_error());
+	mysql_select_db("clasedemo", $conn);
+	$nombre = $_POST['username'];
+	$password = $_POST['password'];
+	$sql= "SELECT * FROM registro WHERE nombre = '$nombre' and password= sha1(md5('$password'))";
+	$result = mysql_query($sql);
+	$count = mysql_num_rows($result);
+	  if ($count == 1) {
+			$_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $nombre;
+            $_SESSION['start'] = time();
+            $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
+			echo "<br> Bienvenido! " . $_SESSION['username'];
+
+	  }	
+      else {
+		echo "<br/>Username o Password estan incorrectos.<br>";
+		echo "<a href='login.html'>Volver a Intentarlo</a>";
+	  }
+
+
+?>
+
+</body>
+</html>
